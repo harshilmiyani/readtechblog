3dda76b6394a5a5a1ec394d634dc14984de64e3b
Harshil Miyani <harshilmiyani2002@gmail.com>
2023-04-07 21:46:56 +0530

    - write first blog

--- src/controller/blog/index.js
+++ src/controller/blog/index.js
@@ -9,13 +9,14 @@ exports.getAllBlog = (req, res, next) => {
 exports.getBlog = (req, res, next) => {
   const name = req.query?.link;
   const blogId = req.query?.blog;
-  const isBlogFileAvailabel= blogs.find((b)=>b.fileName===blogId)
+  const blogDetails= blogs.find((b)=>b.fileName===blogId)
 
   console.log(`Hello, ${blogId}`);
-  if (!!blogId && isBlogFileAvailabel) {
+  if (!!blogId && blogDetails) {
     return res.render(`blog/blogs/${blogId}`, {
       showGetLinkButton: !!name,
       link: "https://t.me/MovieMood1221_bot?start=authToken",
+      blogDetails:blogDetails
     });
   } else {
     res.redirect("blogs");
--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,41 +1,15 @@
 module.exports = [
   {
-    fileName: "you-can-learn-how-to-make-money-with-your-blog-and-videos",
-    author: "Jack",
-    date: "24 JULY, 2017",
-    category: "Marketing",
-    title: "You can learn how to make money with your blog and videos",
-    description:
-      "Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enimcerat elicerat eli nibh, maximus ac felis nec, maximus tempor odio.",
-    imageUrl:
-      "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg",
-    fullBlogLink:
-      "http://localhost:3001/blog?blog=you-can-learn-how-to-make-money-with-your-blog-and-videos",
-  },
-  {
-    fileName: "you-can-learn-how-to-make-money-with-your-blog-and-videos",
+    fileName: "chatgpt-4-the-next-evolution-in-natural-language-processing",
     author: "Harshil",
     date: "24 March, 2023",
-    category: "Make Money",
-    title: "How can you become rich.",
+    category: "Technology",
+    title: "ChatGPT-4: The Next Evolution in Natural Language Processing",
     description:
-      "You can be rich. There is one thing is needed that is you just need to do hard work.",
+      "ChatGPT-4 is the latest and most advanced language model based on the GPT-3 architecture, and it has the potential to revolutionize the way we interact with technology.",
     imageUrl:
-      "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg",
+      "images/blog/chetgpt4.jpg",
     fullBlogLink:
-      "http://localhost:3001/blog?blog=you-can-learn-how-to-make-money-with-your-blog-and-videos",
-  },
-  {
-    fileName: "you-can-learn-how-to-make-money-with-your-blog-and-videos",
-    author: "Rahul",
-    date: "24 February, 2023",
-    category: "Make Money",
-    title: "Do hard work!!",
-    description:
-      "Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enimcerat elicerat eli nibh, maximus ac felis nec, maximus tempor odio.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80",
-    fullBlogLink:
-      "http://localhost:3001/blog?blog=you-can-learn-how-to-make-money-with-your-blog-and-videos",
+      "https://readtechblog/blog?blog=chatgpt-4-the-next-evolution-in-natural-language-processing",
   },
 ];
