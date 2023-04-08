387d6470a022bbb49e9ecc85d045dafa68228553
Harshil Miyani <harshilmiyani2002@gmail.com>
2023-04-08 22:30:49 +0530

    - add pagination in home screen

--- src/controller/home/index.js
+++ src/controller/home/index.js
@@ -1,15 +1,27 @@
 const blogs = require("../../data/blogs");
 exports.getHome = (req, res, next) => {
   const movieId = req.query?.link;
-  console.log(`Hello, ${movieId}!`);
+  const page = req.query?.page ?? 1;
+  const previousPageNumber =
+    +page * 3 - 3 > 0 &&( +page * 3 <= blogs.length || +page * 3 -3 <= blogs.length)? +page * 3 - 3 : 0;
+  const nextPageNumber =
+    blogs.length >= +previousPageNumber + 3
+      ? +previousPageNumber + 3
+      : blogs.length;
+  
+  console.log(`Hello,${previousPageNumber} ${nextPageNumber} `);
   if (movieId) {
     return res.render("blog/singleBlog", {
       showGetLinkButton: !!movieId,
       link: "https://t.me/MovieMood1221_bot?start=authToken",
     });
-  } else {  
+  } else {
     res.render("home/index", {
-      blogs: blogs ?? [],
+      blogs: blogs.slice(previousPageNumber,nextPageNumber) ?? [],
+      page: page ?? 0,
+      previousPageNumber:previousPageNumber??0,
+      nextPageNumber:nextPageNumber ??0,
+      totalBlogs:blogs.length
     });
   }
 };
