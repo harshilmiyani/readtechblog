7997aed6a828c1408b3556d194919eccd3f5dc1f
Harshil Miyani <harshilmiyani2002@gmail.com>
2023-04-08 17:32:37 +0530

    - write new blog on exploring-the-world-of-virtual-and-augmented-reality

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     fileName: "chatgpt-4-the-next-evolution-in-natural-language-processing",
-    author: "Jack",
+    author: "readtechblog",
     date: "7 April, 2023",
     category: "Technology",
     title: "ChatGPT-4: The Next Evolution in Natural Language Processing",
@@ -14,7 +14,7 @@ module.exports = [
   },
   {
     fileName: "the-beginners-guide-to-blockchain-technology",
-    author: "Martin",
+    author: "readtechblog",
     date: "8 April, 2023",
     category: "Technology",
     title: "The Beginner's Guide to Blockchain Technology",
@@ -24,5 +24,18 @@ module.exports = [
       "images/blog/blockChain.jpg",
     fullBlogLink:
       "https://readtechblog.tech/blog?blog=the-beginners-guide-to-blockchain-technology",
+  },
+  {
+    fileName: "exploring-the-world-of-virtual-and-augmented-reality",
+    author: "readtechblog",
+    date: "8 April, 2023",
+    category: "Technology",
+    title: "Exploring the World of Virtual and Augmented Reality",
+    description:
+      "This article explores how virtual and augmented reality technologies are being used in various industries such as gaming, education, retail, and manufacturing. It discusses the potential of these technologies to transform the way we interact with the world and provides examples of their current and future applications.",
+    imageUrl:
+      "images/blog/virtualRealityvsAugmentedReality.jpg",
+    fullBlogLink:
+      "https://readtechblog.tech/blog?blog=exploring-the-world-of-virtual-and-augmented-reality",
   },
 ];
